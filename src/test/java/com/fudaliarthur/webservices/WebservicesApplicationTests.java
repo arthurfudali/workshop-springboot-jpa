d81c8871e15c4d084a3ec87920f1195cab47d5db
arthurfudali <arthur.fudali@gmail.com>
2025-09-24 11:42:01 -0300

    Initial Commit

---
+++ src/test/java/com/fudaliarthur/webservices/WebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fudaliarthur.webservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
