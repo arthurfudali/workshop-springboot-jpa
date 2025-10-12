f6bbfaf06bfe342371558d3761fe0ad98eaba09c
arthurfudali <arthur.fudali@gmail.com>
2025-10-11 22:54:52 -0300

    feat: add DTOs and createOrder method

---
+++ src/main/java/com/fudaliarthur/webservices/dto/OrderRequestDTO.java
@@ -0,0 +1,56 @@
+package com.fudaliarthur.webservices.dto;
+
+import com.fudaliarthur.webservices.entities.enums.OrderStatus;
+
+import java.time.Instant;
+import java.util.List;
+
+public class OrderRequestDTO {
+    private Instant moment;
+    private OrderStatus orderStatus;
+    private Long clientId;
+    private List<OrderItemRequestDTO> items;
+
+    private OrderRequestDTO() {
+    }
+
+    public OrderRequestDTO(Instant moment, OrderStatus orderStatus, Long clientId, List<OrderItemRequestDTO> items) {
+        this.moment = moment;
+        this.orderStatus = orderStatus;
+        this.clientId = clientId;
+        this.items = items;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public Long getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(Long clientId) {
+        this.clientId = clientId;
+    }
+
+    public List<OrderItemRequestDTO> getItems() {
+        return items;
+    }
+
+    public void setItems(List<OrderItemRequestDTO> items) {
+        this.items = items;
+    }
+
+}
--- src/main/java/com/fudaliarthur/webservices/services/OrderService.java
+++ src/main/java/com/fudaliarthur/webservices/services/OrderService.java
@@ -1,10 +1,20 @@
 package com.fudaliarthur.webservices.services;
 
+import com.fudaliarthur.webservices.dto.OrderItemRequestDTO;
+import com.fudaliarthur.webservices.dto.OrderRequestDTO;
 import com.fudaliarthur.webservices.entities.Order;
+import com.fudaliarthur.webservices.entities.OrderItem;
+import com.fudaliarthur.webservices.entities.Product;
+import com.fudaliarthur.webservices.entities.User;
 import com.fudaliarthur.webservices.repositories.OrderRepository;
+import com.fudaliarthur.webservices.repositories.ProductRepository;
+import com.fudaliarthur.webservices.repositories.UserRepository;
+import com.fudaliarthur.webservices.services.exceptions.ResourceNotFoundException;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,14 +22,59 @@ import java.util.Optional;
 public class OrderService {
     @Autowired
     private OrderRepository orderRepository;
+    private UserRepository userRepository;
+    private ProductRepository productRepository;
 
     // repassa a chamada para o repository
-    public List<Order> findAll(){
+    public List<Order> findAll() {
         return orderRepository.findAll();
     }
 
-    public Order findById(Long id){
+    public Order findById(Long id) {
         Optional<Order> obj = orderRepository.findById(id);
         return obj.get();
     }
+
+    @Transactional
+    public Order createOrder(OrderRequestDTO orderRequestDTO) {
+        // busca o cliente no banco
+        User client = userRepository.findById(orderRequestDTO.getClientId())
+                .orElseThrow(() -> new ResourceNotFoundException(orderRequestDTO.getClientId()));
+
+        // criar order
+        Order order = new Order();
+        order.setMoment(orderRequestDTO.getMoment());
+        order.setClient(client);
+        order.setOrderStatus(orderRequestDTO.getOrderStatus());
+
+        // listar e acossiar os OrderItems
+        List<OrderItem> orderItems = new ArrayList<>();
+
+        for (OrderItemRequestDTO itemRequestDTO : orderRequestDTO.getItems()) {
+            Product product = productRepository.findById(itemRequestDTO.getProductId())
+                    .orElseThrow(() -> new ResourceNotFoundException(itemRequestDTO.getProductId()));
+
+            // criar o OrderItem usando o construtor
+            OrderItem orderItem = new OrderItem(
+                    order,           // associa o pedido
+                    product,         // associa o produto
+                    itemRequestDTO.getQuantity(),  // quantidade
+                    itemRequestDTO.getPrice()      // preço
+            );
+
+            orderItems.add(orderItem);
+
+        }
+
+        // associar os itens ao pedido
+        // Como seu Order tem um Set<OrderItem>, precisamos adicionar todos
+        order.getItems().addAll(orderItems);
+
+        // salvar o pedido (funciona por causa do cascade)
+
+        return orderRepository.save(order);
+
+
+    }
+
 }
